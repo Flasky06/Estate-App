5c386b370107e877f2f5fa4eb82cb63fe22c4fac
Flasky06 <bonnienjuguna106@gmail.com>
2023-10-24 00:09:32 +0300

    configured typescript

--- client/src/components/footer/siteBottom.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function siteBottom() {
-	return <div>siteBottom</div>;
-}
-
-export default siteBottom;
--- client/src/create-house/createHouse.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function createHouse() {
-	return <div>createHouse</div>;
-}
-
-export default createHouse;
