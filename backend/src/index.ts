7c9cbaf5f6fc6d9ff99c450fe688b89dae09bcbd
Flasky06 <bonnienjuguna106@gmail.com>
2023-10-25 08:48:55 +0300

    updates

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,6 +4,8 @@ import bodyParser from "body-parser";
 import cookieParser from "cookie-parser";
 import compression from "compression";
 import cors from "cors";
+import mongoose from "mongoose";
+import router from "./router";
 
 const app = express();
 
@@ -13,8 +15,18 @@ app.use(compression());
 app.use(cookieParser());
 app.use(bodyParser.json());
 
+// INITIATING ROUTER
+app.use("/", router());
+
 const server = http.createServer(app);
 
-server.listen(8082, () => {
+// STARTING EXPRESS SERVER
+server.listen(8080, () => {
 	console.log("Server running on http://localhost:8080");
 });
+
+// CONNECTING TO MONGOOSE
+const MONGO_URL = "mongodb+srv://bony:bony@estate.3bbgkgp.mongodb.net/";
+mongoose.Promise = Promise;
+mongoose.connect(MONGO_URL);
+mongoose.connection.on("error", (error: Error) => console.log(error));
