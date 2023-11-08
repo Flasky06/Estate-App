728123887d060da6eb062e3c43fdd13ad3b23b2c
Flasky06 <bonnienjuguna106@gmail.com>
2023-11-09 00:45:25 +0300

    updates

--- frontend/src/components/ListingsCategories.vue
+++ frontend/src/components/ListingsCategories.vue
@@ -3,24 +3,13 @@
 		<h1 class="text-center font-bold text-3xl my-8">Explore Our Listings</h1>
 
 		<div
-			class="grid grid-cols-1 md:grid-cols-2 gap-4 lg:grid-cols-3 lg:gap-8 lg:px-8"
+			v-for="listing in listings"
+			:key="listing.title"
+			class="p-4 rounded-lg shadow-md my-2 mx-1 relative flex flex-col items-center justify-center space-y-3"
 		>
-			<div
-				v-for="data in listings"
-				:key="data.title"
-				class="bg-white p-4 rounded-lg shadow-md flex flex-col items-center"
-			>
-				<img
-					:src="data.image"
-					alt="Listing Image"
-					class="w-full h-40 object-cover rounded-lg mb-2"
-				/>
-				<button
-					class="text-lg font-semibold mx-auto py-2 px-4 underline underline-offset-4"
-				>
-					Discover {{ data.type }}
-				</button>
-			</div>
+			<h3 class="font-semibold text-lg">Discover {{ listing.type }}</h3>
+			<p class="">{{ listing.description }}</p>
+			<button class="bg-white rounded text-black px-2 py-1">Explore</button>
 		</div>
 	</section>
 </template>
@@ -30,40 +19,19 @@ import { ref } from "vue";
 
 const listings = ref([
 	{
-		image:
-			"https://splendorholdings.co.ke/wp-content/uploads/2023/05/Alba-Gardens-Studio-1--1024x684.png",
-		title: "Alba Gardens Studio",
-		type: "studio",
+		type: "For Rent",
+		description:
+			"Explore a wide range of rental and lease properties, available in all sizes and locations, perfectly tailored to your budget. Discover your ideal home today!",
 	},
 	{
-		image:
-			"https://splendorholdings.co.ke/wp-content/uploads/2023/05/Splendor-Holdings-Leshwa-Web-Version18-1024x683.jpg",
-		title: "Splendor Holdings Leshwa",
-		type: "1 Bedroom",
+		type: "For Sale",
+		description:
+			"We offer a variety of properties for sale, available in all sizes and locations, all within your budget. Find your dream home today!",
 	},
 	{
-		image:
-			"https://splendorholdings.co.ke/wp-content/uploads/2023/05/Alba-Gardens-2bd-2-1024x684.png",
-		title: "Alba Gardens 2bd",
-		type: "2 Bedroom",
-	},
-	{
-		image:
-			"https://splendorholdings.co.ke/wp-content/uploads/2023/05/Royal-Oaks-48-1024x683.png",
-		title: "Royal Oaks 48",
-		type: "3 Bedroom",
-	},
-	{
-		image:
-			"https://splendorholdings.co.ke/wp-content/uploads/2023/05/Alba-Gardens-Studio-1--1024x684.png",
-		title: "Alba Gardens Studio",
-		type: "4 Bedroom",
-	},
-	{
-		image:
-			"https://splendorholdings.co.ke/wp-content/uploads/2023/05/Splendor-Holdings-Leshwa-Web-Version18-1024x683.jpg",
-		title: "Splendor Holdings Leshwa",
-		type: "5 Bedroom",
+		type: "Office Spaces",
+		description:
+			"Discover contemporary office spaces available in various locations.",
 	},
 ]);
 </script>
--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -4,6 +4,9 @@
 	<FeaturedProperties />
 
 	<ListingsCategories />
+	Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente velit
+	perferendis quod molestias aliquam, animi nulla vitae recusandae, rem beatae
+	aspernatur! Ratione alias voluptate ab suscipit sequi repellat ducimus nam!
 </template>
 <script setup>
 import FeaturedProperties from "../components/FeaturedProperties.vue";
