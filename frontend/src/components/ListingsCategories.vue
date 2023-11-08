910e769d80cba61f32c9cccadf398181f30d478d
Flasky06 <bonnienjuguna106@gmail.com>
2023-11-09 01:30:16 +0300

    updates

--- frontend/src/components/ListingsCategories.vue
+++ frontend/src/components/ListingsCategories.vue
@@ -1,35 +1,22 @@
 <template>
-	<section
-		class="pt-24 bg-opacity-60 bg-black transition-opacity opacity-80"
-		style="
-			background-image: url(https://cdn.pixabay.com/photo/2016/11/06/23/51/buildings-1804481_640.jpg);
-			background-size: cover;
-			background-position: center;
-		"
-	>
-		<div class="bg-opacity-60 bg-black transition-opacity opacity-80">
-			<h1 class="text-center font-bold text-3xl my-8 text-white">
-				Explore Our Listings
-			</h1>
+	<section class="">
+		<h1 class="text-center font-bold text-3xl my-8">Explore Our Listings</h1>
 
-			<div
-				v-for="listing in listings"
-				:key="listing.title"
-				class="p-4 rounded-lg shadow-sm shadow-blue-400 my-2 mx-2 relative flex flex-col items-center justify-center space-y-3"
+		<div
+			v-for="listing in listings"
+			:key="listing.title"
+			class="p-4 rounded-lg shadow-md my-2 mx-1 relative flex flex-col items-center justify-center space-y-3"
+		>
+			<h3 class="font-semibold text-lg">Discover {{ listing.type }}</h3>
+			<p class="text-center">{{ listing.description }}</p>
+			<button
+				class="bg-blue-600 rounded text-white px-2 py-1 flex items-center cursor-pointer"
 			>
-				<h3 class="font-semibold text-xl text-white">
-					Discover {{ listing.type }}
-				</h3>
-				<p class="text-center text-white text-lg">{{ listing.description }}</p>
-				<button
-					class="bg-blue-600 rounded text-white px-2 py-1 flex items-center cursor-pointer"
-				>
-					<span class="flex items-center text-lg">Explore</span>
-					<span class="material-symbols-outlined text-base">
-						arrow_right_alt
-					</span>
-				</button>
-			</div>
+				<span class="flex items-center text-lg">Explore</span>
+				<span class="material-symbols-outlined text-base">
+					arrow_right_alt
+				</span>
+			</button>
 		</div>
 	</section>
 </template>
