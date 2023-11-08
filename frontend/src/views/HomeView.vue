c2021a87578ec35092cf759f24e129e82a4c22a0
Flasky06 <bonnienjuguna106@gmail.com>
2023-11-09 01:28:12 +0300

    updates

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -4,9 +4,11 @@
 	<FeaturedProperties />
 
 	<ListingsCategories />
-	Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente velit
-	perferendis quod molestias aliquam, animi nulla vitae recusandae, rem beatae
-	aspernatur! Ratione alias voluptate ab suscipit sequi repellat ducimus nam!
+	<p class="mt-40">
+		Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente velit
+		perferendis quod molestias aliquam, animi nulla vitae recusandae, rem beatae
+		aspernatur! Ratione alias voluptate ab suscipit sequi repellat ducimus nam!
+	</p>
 </template>
 <script setup>
 import FeaturedProperties from "../components/FeaturedProperties.vue";
