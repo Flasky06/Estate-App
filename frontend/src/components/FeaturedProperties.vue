ce5803881c695d805150dc2f33821f4019531182
Flasky06 <bonnienjuguna106@gmail.com>
2023-11-10 11:56:43 +0300

    updates

--- frontend/src/components/FeaturedProperties.vue
+++ frontend/src/components/FeaturedProperties.vue
@@ -4,18 +4,21 @@
 			Discover Our Featured Properties
 		</h1>
 		<div class="flex flex-col">
-			<div class="flex flex-col w-full" v-for="property in Properties">
-				<div class="w-full h-60 px-4">
+			<div class="flex flex-col w-full px-4" v-for="property in Properties">
+				<div class="w-full h-60 relative">
 					<img
 						:src="property.src"
 						:alt="property.title"
 						class="w-full h-full"
 					/>
+					<span
+						class="absolute top-1 right-2 bg-opacity-60 bg-black transition-opacity opacity-80 text-white text-lg px-4 py-1"
+						>{{ property.category }}</span
+					>
 				</div>
 
-				<div class="flex flex-col">
-					<h3 class="font-semibold text-xl">{{ property.title }}</h3>
-					<span>{{ property.category }}</span>
+				<div class="flex flex-col px-2">
+					<h3 class="font-light text-2xl">{{ property.title }}</h3>
 
 					<div class="flex space-x-2 items-center">
 						<span class="material-symbols-outlined text-4xl"> bed </span>
@@ -38,7 +41,7 @@ const Properties = ref([
 		src: "https://i.pinimg.com/236x/cb/63/dc/cb63dc6bb5b3e35f2dab28d67dc7d7ae.jpg",
 		alt: "image 1",
 		title: "Gibcon Apartment",
-		category: "for sale",
+		category: "For sale",
 		size: "2 Bedroom ",
 		price: "2000000",
 	},
@@ -46,7 +49,7 @@ const Properties = ref([
 		src: "https://i.pinimg.com/236x/cb/63/dc/cb63dc6bb5b3e35f2dab28d67dc7d7ae.jpg",
 		alt: "image 2",
 		title: "Sigma Apartment",
-		category: "for sale",
+		category: "For sale",
 		size: "3 Bedroom",
 		price: "2000000",
 	},
@@ -54,7 +57,7 @@ const Properties = ref([
 		src: "https://cdn.pixabay.com/photo/2016/10/16/10/29/office-space-1744801_640.jpg",
 		alt: "image 3",
 		title: "Alphine House",
-		category: "for sale",
+		category: "For sale",
 		size: "Studio",
 		price: "20000",
 	},
@@ -62,15 +65,15 @@ const Properties = ref([
 		src: "https://cdn.pixabay.com/photo/2016/11/29/04/57/architecture-1867426_640.jpg",
 		alt: "image 4",
 		title: "Green Park",
-		category: "for rent",
+		category: "For rent",
 		size: "1 Bedroom",
 		price: "42000",
 	},
 	{
 		src: "https://cdn.pixabay.com/photo/2017/03/30/00/24/villa-2186906_640.jpg",
 		alt: "image 5",
-		title: "Rodridge",
-		category: "for rent",
+		title: "Rodridge Apartment",
+		category: "For rent",
 		size: "2 Bedroom",
 		price: "55000",
 	},
@@ -78,7 +81,7 @@ const Properties = ref([
 		src: "https://cdn.pixabay.com/photo/2019/03/08/20/14/kitchen-living-room-4043091_640.jpg",
 		alt: "image 6",
 		title: "Kelani Estate",
-		category: "for sale",
+		category: "For sale",
 		size: "3 Bedroom ",
 		price: "4500000",
 	},
